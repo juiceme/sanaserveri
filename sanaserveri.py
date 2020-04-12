39b6698d77df5e64d4e25c76bbd841d6c7a62699
Jussi Ohenoja <juice@swagman.org>
2020-04-12 23:19:28 +0300

    Add https server functionality

--- sanaserveri.py
+++ sanaserveri.py
@@ -1,3 +1,7 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+import ssl
+
 from wordlist import WordList as wl
 from wordtable import WordTable as wt
 
@@ -13,3 +17,39 @@ table.print_table()
 print()
 table.fill_table()
 table.print_table()
+
+def handle_rest_get(path):
+    if path == "/getwords":
+        return table.get_raw_table()
+    return b"{status: OK}"
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(handle_rest_get(self.path))
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+    def do_PUT(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is PUT request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+httpd = HTTPServer(('localhost', 8088), MyHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="/home/juice/key.pem",
+                               certfile="/home/juice/cert.pem", server_side=True)
+httpd.serve_forever()
