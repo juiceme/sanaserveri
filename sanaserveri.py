1f08c87709234e03f8acc6e17ce3672bee54869d
Jussi Ohenoja <juice@swagman.org>
2020-04-13 19:10:45 +0300

    Add rest put handling

--- sanaserveri.py
+++ sanaserveri.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 import ssl
+import json
 
 from wordlist import WordList as wl
 from wordtable import WordTable as wt
@@ -20,8 +21,23 @@ table.print_table()
 
 def handle_rest_get(path):
     if path == "/getwords":
-        return table.get_raw_table()
-    return b"{status: OK}"
+        ret = { "table": table.get_raw_table(),
+                "status": "OK" }
+        return json.dumps(ret).encode('ascii')
+    return json.dumps({"status": "FAIL"}).encode('ascii')
+
+def handle_rest_put(path, body):
+    if path == "/checkword":
+        data = json.loads(body)
+        print(data["word"])
+        if table.check_validity(json.loads(data["word"])):
+            word = table.get_word(json.loads(data["word"]))
+            print(word)
+            if len(word) < 3 or len(word) > 10:
+                return json.dumps({"status": "FAIL"}).encode('ascii')
+            if wordlists[len(word)-3].is_word(word):
+                return json.dumps({"status": "OK"}).encode('ascii')
+    return json.dumps({"status": "FAIL"}).encode('ascii')
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -44,9 +60,7 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
-        response.write(b'This is PUT request. ')
-        response.write(b'Received: ')
-        response.write(body)
+        response.write(handle_rest_put(self.path, body))
         self.wfile.write(response.getvalue())
 
 httpd = HTTPServer(('localhost', 8088), MyHTTPRequestHandler)
