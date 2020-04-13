6961eece447cfbda01afc19e36a9946aa02785f2
Jussi Ohenoja <juice@swagman.org>
2020-04-14 00:07:05 +0300

    Add session handling

--- sanaserveri.py
+++ sanaserveri.py
@@ -2,6 +2,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 import ssl
 import json
+import hashlib
+import time
+import random
 
 from wordlist import WordList as wl
 from wordtable import WordTable as wt
@@ -19,25 +22,54 @@ print()
 table.fill_table()
 table.print_table()
 
+sessions = []
+
+def create_session():
+    key = hashlib.sha1(str(time.time()+random.randrange(1000)).encode('utf-8')).hexdigest()
+    sessions.append({"key": key, "score": 0})
+    return { "key": key, "status": "OK" }
+
+def get_session(key):
+    session = ""
+    for i in sessions:
+        if i["key"] == key:
+            session = i
+            break
+    return session
+
 def handle_rest_get(path):
     if path == "/getwords":
         ret = { "table": table.get_raw_table(),
                 "status": "OK" }
         return json.dumps(ret).encode('ascii')
-    return json.dumps({"status": "FAIL"}).encode('ascii')
+    if path == "/startsession":
+        return json.dumps(create_session()).encode('ascii')
+    return json.dumps({"status": "FAIL", "error": "Invalid path"}).encode('ascii')
 
 def handle_rest_put(path, body):
+    data = json.loads(body)
+    if "key" not in data:
+        return json.dumps({"status": "FAIL", "error": "Invalid format"}).encode('ascii')
     if path == "/checkword":
-        data = json.loads(body)
-        print(data["word"])
+        if "word" not in data:
+            return json.dumps({"status": "FAIL", "error": "Invalid format"}).encode('ascii')
+        session = get_session(data["key"])
+        if session == "":
+            return json.dumps({"status": "FAIL", "error": "Unknown session"}).encode('ascii')
         if table.check_validity(json.loads(data["word"])):
             word = table.get_word(json.loads(data["word"]))
-            print(word)
             if len(word) < 3 or len(word) > 10:
-                return json.dumps({"status": "FAIL"}).encode('ascii')
+                return json.dumps({"status": "FAIL", "error": "Invalid word"}).encode('ascii')
             if wordlists[len(word)-3].is_word(word):
-                return json.dumps({"status": "OK"}).encode('ascii')
-    return json.dumps({"status": "FAIL"}).encode('ascii')
+                session["score"] = session["score"] + len(word) * len(word)
+                return json.dumps({"score": session["score"], "status": "OK"}).encode('ascii')
+        return json.dumps({"status": "FAIL", "error": "Not a word"}).encode('ascii')
+    if path == "/getsessions":
+        session = get_session(data["key"])
+        if session == "":
+            return json.dumps({"status": "FAIL", "error": "Unknown session"}).encode('ascii')
+        return json.dumps({"sessions": sessions, "status": "OK"}).encode('ascii')
+    return json.dumps({"status": "FAIL", "error": "Invalid path"}).encode('ascii')
 
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -45,14 +77,10 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(handle_rest_get(self.path))
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
         self.send_response(200)
         self.end_headers()
         response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
+        response.write(json.dumps({"status": "FAIL"}).encode('ascii'))
         self.wfile.write(response.getvalue())
     def do_PUT(self):
         content_length = int(self.headers['Content-Length'])
